afb7c607c9eb28dc7a8f13d6e24450174d937a09
Derick Daniel <derickdaniel44@gmail.com>
2024-07-01 02:38:34 +0530

    Task UI changes

--- js/note.js
+++ js/note.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
+	getTasks();
+	getTodaysTasks();
 	$("#saveBtn").hide();
-	
+
 	$("#add").on("click", function() { //hadle click and "Enter" event
 		var val = $("input").val(); //get user input value
 
 		if (val !== '') {
-			var elem = $("<li></li>").text(val); //create new element
-			$(elem).append("<button class='rem'>X</button>"); //add x button to new element
-			$("#mylist").append(elem); //add new element at the end of my list
-			$("input").val(""); //clear the input
-			$(".rem").on("click", function() { //handle click on x button
-				$(this).parent().remove(); //remove "current => this" object
-				if ($("#mylist li").length == 0) {
-					$("#saveBtn").hide();
-				}
-			});
+			addNewTaskToList(null, val, false);
 			$("#saveBtn").show();
 		}
 	});
@@ -24,4 +17,131 @@ $(document).ready(function() {
 			$('#add').click();//Trigger Add button click event
 		}
 	});
+
+	$(document).on('click', ".rem", function() { //handle click on x button
+		$(this).parent().parent().remove(); //remove "current => this" object
+		if ($("#mylist li").length == 0) {
+			$("#saveBtn").hide();
+		}
+	});
+	
+	$(document).on("click", ".tick", function() {
+		if ($(this).parent().parent().attr('class') == 'compl') {
+			$(this).parent().parent().removeClass();
+			$("#doneVal").val(false);
+		} else {
+			$(this).parent().parent().addClass("compl");
+			$("#doneVal").val(true);			
+		}
+	});
+
+	//save to db
+	$("#saveAllId").on("click", function() {
+		var jsonData = JSON.stringify(createTaskJson());
+		console.log(jsonData);
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:8080/dib/task",
+			data: jsonData,
+			contentType: "application/json; charset=UTF-8",
+			headers: {
+				"Authorization": "Bearer " + localStorage.getItem('access-token'),
+			},
+			success: function(response) {
+				getTasks();
+			},
+			error: function(response, error) {
+				console.log("request failed: " + response.status);
+				if (response.status == 401) {
+					location.href = "login.html";
+				}
+			}
+		});
+	});
 });
+
+function getTasks() {
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:8080/dib/tasks",
+		contentType: "application/json; charset=UTF-8",
+		dataType: 'json',
+		headers: {
+			"Authorization": "Bearer " + localStorage.getItem('access-token'),
+		},
+		success: function(response) {
+			console.log(response);
+			var elem = "";
+			$.each(response, function(k, v) {
+				elem += "<h3>" + k + "</h3>";
+				elem += "<ul>"
+				v.forEach(function(task, index) {
+					elem += "<li>" + task.taskDesc + "</li>"
+				});
+				elem += "</ul>"
+
+			});
+			$("#taskResult").html(elem);
+		},
+		error: function(response, error) {
+			console.log("request failed: " + response.status);
+			if (response.status == 401) {
+				location.href = "login.html";
+			}
+		}
+	});
+}
+
+function getTodaysTasks() {
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:8080/dib/tasks/today",
+		contentType: "application/json; charset=UTF-8",
+		dataType: 'json',
+		headers: {
+			"Authorization": "Bearer " + localStorage.getItem('access-token'),
+		},
+		success: function(response) {
+			console.log(response);
+			if (response.length > 0) {
+				$("#saveBtn").show();
+				response.forEach(function(task, index) {
+					addNewTaskToList(task.taskId, task.taskDesc, task.completed);
+					if (task.completed) {
+						
+					}
+				});
+			}
+		},
+		error: function(response, error) {
+			console.log("request failed: " + response.status);
+			if (response.status == 401) {
+				location.href = "login.html";
+			}
+		}
+	});
+}
+
+function createTaskJson() {
+	noteArr = [];
+	var listItems = $("#mylist li");
+	if (listItems.length > 0) {
+		listItems.each(function(idx, li) {
+			noteArr[idx] = {
+				taskDesc: $(li).children().first().text().trim(),
+				completed: ($(li).children().first().next().val() === 'true'),
+				taskId: $(li).attr('id')
+			};
+		});
+	}
+	return { tasks: noteArr };
+}
+
+function addNewTaskToList(id, val, isDone) {
+	var elem = $('<li id="'+ id +'"></li>'); //create new element
+	$(elem).append("<span>" + val + " </span>");
+	$(elem).append('<input id="doneVal" type="hidden" value="'+ isDone +'"/>')
+	$(elem).append('<div style="display: inline-flex;"><input id="cb1" type="checkbox" onclick="return false" checked=checked class="tick"/> <button id="xid" class="rem">X</button></div>');
+	$("#mylist").append(elem); //add new element at the end of my list
+	$("input").val("");
+}
